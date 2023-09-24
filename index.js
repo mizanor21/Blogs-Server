ddf0f1050c0bec2f4d2a52148d825ddac0cbd5d1
mizan21 <mizanorrahman398@gmail.com>
2023-09-24 09:58:07 +0600

    Vercel deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { ObjectId } = require("mongodb"); // Import ObjectId from mongodb library
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
